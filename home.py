d013e4a106ab492fadd3a174208dd7edb8246463
Carlos Santos <carlos.santosx85@hotmail.com>
2025-05-29 19:52:47 -0300

    Add files via upload

--- home.py
+++ home.py
@@ -22,12 +22,10 @@ def home_page():
                 time.sleep(2)  # Delay de 1 segundo 
                 atualizar_email_sn_para_s1(usuario["seq"])
                 time.sleep(2)  # Delay de 1 segundo 
-
             st.success("Agora você está participando!")
-            st.balloons()
-            
+            st.balloons()            
             st.rerun()
     elif email_sn == "S":
-        st.success("Você já está participando!")
+        st.success("")
     else:
         st.error("Não foi possível verificar seu status de participação.")
