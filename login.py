930fa69b36381611b07eaa273f03fedfb06e5eb8
Carlos Santos <carlos.santosx85@hotmail.com>
2025-05-29 18:41:05 -0300

    Add files via upload

--- login.py
+++ login.py
@@ -7,14 +7,12 @@ def login_page():
     mostrar_cabecalho_publico()
     
     email = st.text_input("Email")
-    senha = st.text_input("Senha", type="password")
-    
+    senha = st.text_input("Senha", type="password")   
     
 
     if st.button("Entrar"):
         if validar_login(email, senha):
             st.success("Login realizado com sucesso!")
-            st.experimental_rerun()  # <- só roda depois que tudo deu certo
+            st.st.rerun() # <- só roda depois que tudo deu certo
         else:
             st.error("Email ou senha inválidos.")
-
