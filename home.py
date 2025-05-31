99e8713aeb4a6ecf4924b7237bae41079af05034
Carlos Santos <carlos.santosx85@hotmail.com>
2025-05-31 14:19:36 -0300

    Add files via upload

--- home.py
+++ home.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import time
-from db import verificar_email_sn, atualizar_email_sn_para_s,atualizar_email_sn_para_s1,buscar_jogos_ativos
+from db import verificar_email_sn, atualizar_email_sn_para_s,atualizar_email_sn_para_s1,buscar_jogos_ativos,atualizar_placar
 
 def home_page():
     if "usuario_logado" not in st.session_state:
@@ -49,7 +49,7 @@ def home_page():
          
 
         # Interface principal
-        st.title("Atualização de Placar - Rodada Ativa")
+
 
         jogos = buscar_jogos_ativos(usuario["seq"])
 
