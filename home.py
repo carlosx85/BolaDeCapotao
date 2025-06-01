fd0c7583757b48f59f75935600c543ddde27d968
Carlos Santos <carlos.santosx85@hotmail.com>
2025-06-01 11:33:02 -0300

    Add files via upload

--- home.py
+++ home.py
@@ -58,7 +58,7 @@ def home_page():
             st.warning("Nenhum jogo ativo encontrado.")
         else:
             # Cabeçalhos da "tabela"
-            st.markdown(f"### Jogos Ativos {jogos["Rodada"]}")
+            st.markdown(f"### Jogos Ativos {jogo["Rodada"]}")
             
             for i, jogo in enumerate(jogos, start=1):
                     
