41bc2336c40740e88a876ec9a84799a3773d1b90
Carlos Santos <carlos.santosx85@hotmail.com>
2025-06-02 15:38:28 -0300

    Add files via upload

--- home.py
+++ home.py
@@ -120,15 +120,16 @@ def home_page():
                                     sucesso  = atualizar_placar_pendente(seq, jogo_id, novo_mandante_gol, novo_visitante_gol)
                                     sucessox = atualizar_placar_pendente_palpite()
                                     
-                                     # 🧹 Limpar campos após salvar
-                                    st.session_state[mandante_key] = ""
-                                    st.session_state[visitante_key] = ""
-                                    
-                                    st.rerun() 
+
+                          
                                     if sucesso:
                                         st.success("✅ Placar atualizado com sucesso!")
                                         st.rerun()  
-                                        st.session_state["pagina"] = "home" 
+                                                                                # 🧹 Limpar campos após salvar
+                                        st.session_state[mandante_key] = ""
+                                        st.session_state[visitante_key] = ""
+                                            
+                                         
                                         
 
 
