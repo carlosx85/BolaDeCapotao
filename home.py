399c1323540f19e433b533a7b4ab6a9ce340f945
Carlos Santos <carlos.santosx85@hotmail.com>
2025-06-02 17:41:27 -0300

    Add files via upload

--- home.py
+++ home.py
@@ -118,7 +118,7 @@ def home_page():
                                     
            
                                     
-                                    st.rerun() 
+                               
                                     if sucesso:
                                         st.success("✅ Placar atualizado com sucesso!")
                                         
