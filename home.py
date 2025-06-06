0a92d741b554a8fd2794dd15ee241db2bf33c536
Carlos Santos <carlos.santosx85@hotmail.com>
2025-06-06 18:06:54 -0300

    Add files via upload

--- home.py
+++ home.py
@@ -63,18 +63,13 @@ def home_page():
             rodadaativa = verificar_rodada_ativa(usuario["seq"])
             rodada_ativa = rodadaativa["Rodada"]
             
-            redadainicio = rodada_inicio(usuario["seq"],rodada_ativa)               
+            
+            redadainicio = rodada_inicio(usuario["seq"],rodada_ativa)   
+            rodada_ativa = redadainicio["Rodada"]            
            
             
 
             st.markdown(f"### Rodada  Ativaxx{rodada_ativa}")  
-            
-            
-             
- 
-                
-                
-                      
             
             jogosx = buscar_jogos_ativos_preenchido(usuario["seq"])               
 
