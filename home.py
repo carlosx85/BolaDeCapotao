289cbd9c892c30320cd2ee53634f955541b5cea7
Carlos Santos <carlos.santosx85@hotmail.com>
2025-06-06 17:59:57 -0300

    Add files via upload

--- home.py
+++ home.py
@@ -68,7 +68,7 @@ def home_page():
             mandante_gol = redadainicio["Mandante_Gol"]
 
             # Verificação
-            if redadainicio["Mandante_Gol"] is None or pd.isna(redadainicio["Mandante_Gol"]):
+            if mandante_gol is None or pd.isna(redadainicio["Mandante_Gol"]):
                 st.write("Fim de Arquivo")
             else:
                 st.write("Início de Arquivo")
@@ -76,33 +76,33 @@ def home_page():
                 
                 
                             
-                st.markdown(f"### Rodada  Ativaxx{rodada_ativa}")  
-                    
-                    
-                        
+            st.markdown(f"### Rodada  Ativaxx{rodada_ativa}")  
                 
-                jogosx = buscar_jogos_ativos_preenchido(usuario["seq"])               
+                
+                      
+            
+            jogosx = buscar_jogos_ativos_preenchido(usuario["seq"])               
 
-                for jogo in jogosx:
-                    pontos = jogo["Pontos"]
-                    mandante = jogo["Mandante"]
-                    visitante = jogo["Visitante"] 
-                    palpite_mandante_gol = jogo["Palpite_Mandante_Gol"] if jogo["Palpite_Mandante_Gol"] is not None else "-"
-                    palpite_visitante_gol = jogo["Palpite_Visitante_Gol"] if jogo["Palpite_Visitante_Gol"] is not None else "-"
-                    mandante_gol = jogo["Mandante_Gol"] if jogo["Mandante_Gol"] is not None else ""
-                    visitante_gol = jogo["Visitante_Gol"] if jogo["Visitante_Gol"] is not None else ""
-                    st.markdown(
-                        f"""
-                        <div style="display: flex; align-items: center; justify-content: center; gap: 10px; margin-bottom: 10px;">
-                            <span style="font-size: 26px; font-weight: ;">{mandante_gol} x {visitante_gol}</span>
-                            <img src="https://boladecapotao.com/times/{mandante.lower()}.png" width="30" />
-                            <span style="font-size: 26px; font-weight: ;">{palpite_mandante_gol} x {palpite_visitante_gol}</span>
-                            <img src="https://boladecapotao.com/times/{visitante.lower()}.png" width="30" />
-                            <span style="font-size: 26px; font-weight: ;">  {pontos}</span>
-                        </div>
-                        """,
-                        unsafe_allow_html=True
-        )
+            for jogo in jogosx:
+                pontos = jogo["Pontos"]
+                mandante = jogo["Mandante"]
+                visitante = jogo["Visitante"] 
+                palpite_mandante_gol = jogo["Palpite_Mandante_Gol"] if jogo["Palpite_Mandante_Gol"] is not None else "-"
+                palpite_visitante_gol = jogo["Palpite_Visitante_Gol"] if jogo["Palpite_Visitante_Gol"] is not None else "-"
+                mandante_gol = jogo["Mandante_Gol"] if jogo["Mandante_Gol"] is not None else ""
+                visitante_gol = jogo["Visitante_Gol"] if jogo["Visitante_Gol"] is not None else ""
+                st.markdown(
+                    f"""
+                    <div style="display: flex; align-items: center; justify-content: center; gap: 10px; margin-bottom: 10px;">
+                        <span style="font-size: 26px; font-weight: ;">{mandante_gol} x {visitante_gol}</span>
+                        <img src="https://boladecapotao.com/times/{mandante.lower()}.png" width="30" />
+                        <span style="font-size: 26px; font-weight: ;">{palpite_mandante_gol} x {palpite_visitante_gol}</span>
+                        <img src="https://boladecapotao.com/times/{visitante.lower()}.png" width="30" />
+                         <span style="font-size: 26px; font-weight: ;">  {pontos}</span>
+                    </div>
+                    """,
+                    unsafe_allow_html=True
+    )
 
 
 
