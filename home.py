7e912ae13ac871cc1b6a154fb6dfeb8696d2d520
Carlos Santos <carlos.santosx85@hotmail.com>
2025-06-06 18:02:07 -0300

    Add files via upload

--- home.py
+++ home.py
@@ -73,14 +73,7 @@ def home_page():
              
             mandante_gol = redadainicio["Mandante_Gol"]
 
-            # Verificação
-            if mandante_gol is None or pd.isna(mandante_gol):
-                st.write("Fim de Arquivo")
-            else:
-                st.write("Início de Arquivo")
-                
-                
-                
+    )
                             
             st.markdown(f"### Rodada  Ativaxx{rodada_ativa}")  
                 
