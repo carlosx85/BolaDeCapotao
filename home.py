c9e45edb7e5db44889cb2a41c489d3a8e4a79b0a
Carlos Santos <carlos.santosx85@hotmail.com>
2025-06-07 09:30:48 -0300

    Add files via upload

--- home.py
+++ home.py
@@ -63,10 +63,7 @@ def home_page():
             rodadaativa = verificar_rodada_ativa(usuario["seq"])
             rodada_ativa = rodadaativa["Rodada"]
             
-            
-            rodadainicio = rodada_inicio(usuario["seq"],rodada_ativa) 
-        
-            rodada_inicio = rodadainicio["Rodada"] 
+         
               
                    
            
