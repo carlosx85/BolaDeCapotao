583b9e5358314aedd4a47476a827b22ecab60f65
Carlos Santos <carlos.santosx85@hotmail.com>
2025-06-11 09:39:33 -0300

    Add files via upload

--- home.py
+++ home.py
@@ -36,8 +36,7 @@ def home_page():
                 time.sleep(2)
                 
                 my_bar.empty()
-            st.success("Agora você está participando!" )
-            st.balloons()            
+            st.success("Agora você está participando!" )           
             st.rerun()          
             
             
