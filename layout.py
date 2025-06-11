c84c363ddc7ee5e0376617f75f894bce6c979d10
Carlos Santos <carlos.santosx85@hotmail.com>
2025-06-11 09:49:38 -0300

    Add files via upload

--- layout.py
+++ layout.py
@@ -1,12 +1,14 @@
 import streamlit as st
 
-def mostrar_cabecalho_publico():    
-        st.markdown(
-            """
-            <div style="text-align: center;">
-                <img src="https://boladecapotao.com/bet/images/BolaDeCapotao.png" width="150">
-                <p style="font-size: 12px; margin-top: 5px;">Bola de Capotão</p>
-            </div>
-            """,
-            unsafe_allow_html=True
-        )
+ 
+def mostrar_cabecalho_publico():
+     
+    
+    header_html = f"""
+    <div style="display: flex; align-items: center; padding: 10px 0 5px 0;">
+        <img src="https://boladecapotao.com/bet/images/BolaDeCapotao.png" width="50" style="margin-right: 15px;">
+        
+    </div>
+    <hr style="margin-top: 5px; margin-bottom: 15px;">
+    """
+    st.markdown(header_html, unsafe_allow_html=True)
