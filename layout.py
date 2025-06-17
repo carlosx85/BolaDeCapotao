cfb412b580c842bcf5c82286fb664d0bcdb6ed88
Carlos Santos <carlos.santosx85@hotmail.com>
2025-06-17 10:12:30 -0300

    Add files via upload

--- layout.py
+++ layout.py
@@ -7,7 +7,7 @@ def mostrar_cabecalho_publico(usuario):
     header_html = f"""
     <div style="display: flex; align-items: center; padding: 10px 0 5px 0;">
         <img src="https://boladecapotao.com/bet/images/BolaDeCapotao.png" width="50" style="margin-right: 15px;">
-        <p style="margin: 0; font-size: 24px;">Bola de Capotão - Palpies >>> {usuario["nome"]} | {usuario["seq"]}</p>
+        <p style="margin: 0; font-size: 20px;">Bola de Capotão - Palpites >>> {usuario["nome"]} | {usuario["seq"]}</p>
     </div>
     <hr style="margin-top: 5px; margin-bottom: 15px;">
     """
