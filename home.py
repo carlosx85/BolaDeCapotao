9341cd3445abd1335e5c6179af07378358ab5f82
Carlos Santos <carlos.santosx85@hotmail.com>
2025-07-14 15:34:52 -0300

    Add files via upload

--- home.py
+++ home.py
@@ -139,11 +139,11 @@ def home_page():
                     st.markdown(
                         f"""
                         <div style="display: flex; align-items: center; justify-content: center; gap: 10px; margin-bottom: 10px;">
-                            <span style="font-size: 26px; font-weight: ; text-align: center;">{mandante_gol} x {visitante_gol}</span>
+                            <span style="font-size: 26px; font-weight: ; text-align: center;">{mandante_gol} x {visitante_gol}&nbsp; &nbsp; </span>
                             <img src="https://boladecapotao.com/times/{mandante.lower()}.png" width="30"  />
                             <span style="font-size: 26px; font-weight: ;">{palpite_mandante_gol} x {palpite_visitante_gol}</span>
                             <img src="https://boladecapotao.com/times/{visitante.lower()}.png" width="30" />
-                            <span style="font-size: 26px; font-weight: ; text-align: center;"> &nbsp; &nbsp; &nbsp; {pontos if pontos is not None else ""} </span> 
+                            <span style="font-size: 26px; font-weight: ; text-align: center;"> &nbsp; &nbsp; &nbsp; &nbsp; {pontos if pontos is not None else ""} </span> 
                         </div>
                         """,
                         unsafe_allow_html=True
