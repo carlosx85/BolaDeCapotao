8f8efae530fc66764bf4a8d8bb37fe40bf0c0912
Carlos Santos <carlos.santosx85@hotmail.com>
2025-07-14 14:46:33 -0300

    Add files via upload

--- db.py
+++ db.py
@@ -2,6 +2,8 @@ import mysql.connector
 import streamlit as st
 import pandas as pd
 import unicodedata
+import requests
+from bs4 import BeautifulSoup
 
 
 def conectar():
@@ -195,6 +197,46 @@ def rodada_inicio_ativar():
 
 def atualizar_online():
     st.write("xxxx")
+    
+    Rodada_Atual=1
+
+    urlx= f'https://www.api-futebol.com.br/campeonato/campeonato-brasileiro/2025'
+    url= f'https://www.api-futebol.com.br/campeonato/campeonato-brasileiro/2025/rodada/{Rodada_Atual}'
+
+    headers ={}
+    response = requests.get(url,headers=headers)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    import pandas as pd
+
+    # Supondo que as variáveis já estejam preenchidas com os elementos extraídos pelo BeautifulSoup
+    Mandante   = soup.find_all('div', {'class': 'text-right'})
+    Visitante  = soup.find_all('div', {'class': 'text-left'})
+    Placar     = soup.find_all('div', {'class': 'small text-center'})
+
+    # Extrair o texto de cada elemento encontrado
+    mandante_list = [element.get_text(strip=True) for element in Mandante if element.get_text(strip=True) not in ['', 'N/A']]
+    visitante_list = [visitante.get_text(strip=True) for visitante in Visitante]
+    placar_list = [placar.get_text(strip=True) for placar in Placar]
+    max_length = max(len(mandante_list), len(visitante_list), len(placar_list))
+
+
+    # Preencher listas menores com valores vazios para igualar o comprimento
+    mandante_list  += [''] * (max_length - len(mandante_list))
+    visitante_list += [''] * (max_length - len(visitante_list))
+    placar_list    += [''] * (max_length - len(placar_list))
+
+    # Criar um dicionário com as listas ajustadas para formar um DataFrame
+    data = {
+        'Mandante': mandante_list,
+        'Placar': placar_list,
+        'Visitante': visitante_list,
+        'Rodada':  Rodada_Atual  
+    }
+
+    # Criar um DataFrame com as informações
+    df = pd.DataFrame(data)
+
 
 
 
