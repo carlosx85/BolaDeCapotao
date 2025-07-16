8bf3601547be89511a935ed9cbe8cddeae6372a9
Carlos Santos <carlos.santosx85@hotmail.com>
2025-07-16 17:30:47 -0300

    Add files via upload

--- layout.py
+++ layout.py
@@ -13,14 +13,13 @@ from db import  Info_Cabecalho
 def mostrar_cabecalho_publico(usuario):
     
  
-    usuario_seq = st.session_state["usuario_logado"]
-    info = Info_Cabecalho(usuario_seq) 
+ 
        
     
     header_html = f"""
     <div style="display: flex; align-items: center; padding: 10px 0 5px 0;">
         <img src="https://boladecapotao.com/bet/images/BolaDeCapotao.png" width="50" style="margin-right: 15px;">
-        <p style="margin: 0; font-size: 20px;">{usuario["evento"]} - {usuario["nome"]} </p>
+        <p style="margin: 0; font-size: 20px;">{usuario["evento"]} - {usuario["nome"]} {usuario["seq"]} </p>
          <h2 style="margin: 0; font-size: 20px;">    # 🏆  º  (150)pts</h2>
     </div>
     <hr style="margin-top: 5px; margin-bottom: 15px;">
