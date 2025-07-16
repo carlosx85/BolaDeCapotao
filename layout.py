c9d7841e05d5ee8eab5812f82dc1522b229b5d88
Carlos Santos <carlos.santosx85@hotmail.com>
2025-07-16 17:21:39 -0300

    Add files via upload

--- layout.py
+++ layout.py
@@ -2,17 +2,24 @@ import streamlit as st
 from db import  Info_Cabecalho
 
  
+
+
+
  
 
  
 def mostrar_cabecalho_publico(usuario):
-     
+    
+    usuario_seq = {usuario["seq"]}
+    info = Info_Cabecalho(usuario_seq) 
+    
+       
     
     header_html = f"""
     <div style="display: flex; align-items: center; padding: 10px 0 5px 0;">
         <img src="https://boladecapotao.com/bet/images/BolaDeCapotao.png" width="50" style="margin-right: 15px;">
-        <p style="margin: 0; font-size: 20px;">{usuario["evento"]} - {usuario["nome"]} {usuario["seq"]}</p>
-         <h2 style="margin: 0; font-size: 20px;">    # 🏆 1º  (150)pts</h2>
+        <p style="margin: 0; font-size: 20px;">{usuario["evento"]} - {usuario["nome"]} </p>
+         <h2 style="margin: 0; font-size: 20px;">    # 🏆 {info["Rank"]}º  (150)pts</h2>
     </div>
     <hr style="margin-top: 5px; margin-bottom: 15px;">
     """
