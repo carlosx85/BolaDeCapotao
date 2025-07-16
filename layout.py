f8d78f47431b258656e4561ab150f5dd508b12bf
Carlos Santos <carlos.santosx85@hotmail.com>
2025-07-16 17:37:17 -0300

    Add files via upload

--- layout.py
+++ layout.py
@@ -12,11 +12,15 @@ from db import  Info_Cabecalho
  
 def mostrar_cabecalho_publico(usuario):
     
-    # Supondo que você já tem o dicionário `usuario`
+    # Pega o seq do usuário passado
     seq_usuario = usuario["seq"]
 
-    # Busca os dados completos do usuário com base no seq
+    # Busca as informações completas do usuário
     info = Info_Cabecalho(seq_usuario)
+
+    # Exemplo: Rank e Pontos (você deve ajustar isso se quiser valores reais)
+    rank = info.get("Rank", "?")     # se não existir, mostra "?"
+    pontos = info.get("Pontos", "???")  # idem
  
        
     
@@ -24,7 +28,7 @@ def mostrar_cabecalho_publico(usuario):
     <div style="display: flex; align-items: center; padding: 10px 0 5px 0;">
         <img src="https://boladecapotao.com/bet/images/BolaDeCapotao.png" width="50" style="margin-right: 15px;">
         <p style="margin: 0; font-size: 20px;">{usuario["evento"]} - {usuario["nome"]} {usuario["seq"]} </p>
-         <h2 style="margin: 0; font-size: 20px;">    # 🏆  {info['Rank']} º  (150)pts</h2>
+         <h2 style="margin: 0; font-size: 20px;">    # 🏆  {rank} º  {pontos}pts</h2>
     </div>
     <hr style="margin-top: 5px; margin-bottom: 15px;">
     """
