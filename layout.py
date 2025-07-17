a67a03e193389f0ede10f4d15ed18585cf644e55
Carlos Santos <carlos.santosx85@hotmail.com>
2025-07-17 11:38:47 -0300

    Add files via upload

--- layout.py
+++ layout.py
@@ -24,12 +24,12 @@ def mostrar_cabecalho_publico(usuario):
      
  
        
-    
+     
     header_html = f"""
     <div style="display: flex; align-items: center; padding: 10px 0 5px 0;">
         <img src="https://boladecapotao.com/bet/images/BolaDeCapotao.png" width="50" style="margin-right: 15px;">
         <p style="margin: 0; font-size: 20px;">{usuario["evento"]} - {usuario["nome"]} {usuario["seq"]} </p>
-         <h2 style="margin: 0; font-size: 20px;">    # 🏆  {rank}º  {pontos} pts</h2>
+         <h2 style="margin: 0; font-size: 20px;">    # 🏆 º   pts</h2>
     </div>
     <hr style="margin-top: 5px; margin-bottom: 15px;">
     """
