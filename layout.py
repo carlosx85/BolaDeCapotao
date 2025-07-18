c2dd2769896549f2e896ed9852ebf99130e5eba2
Carlos Santos <carlos.santosx85@hotmail.com>
2025-07-18 20:13:26 -0300

    Add files via upload

--- layout.py
+++ layout.py
@@ -16,7 +16,7 @@ def mostrar_cabecalho_publico(usuario):
     
     
     rodadax = Info_Rodada()
-    rodada = Info_Rodada["Rodada"] 
+    rodada = rodadax["Rodada"] 
      
  
        
@@ -25,7 +25,7 @@ def mostrar_cabecalho_publico(usuario):
     <div style="display: flex; align-items: center; padding: 10px 0 5px 0;">
         <img src="https://boladecapotao.com/bet/images/BolaDeCapotao.png" width="50" style="margin-right: 15px;">
         <p style="margin: 0; font-size: 20px;">{usuario["evento"]} - {usuario["nome"]}   </p>
-          <h2 style="margin: 0; font-size: 20px;">       🏆 #{rank}º [{pontos}] pts   {Info_Rodada["Rodada"]}</h2>
+          <h2 style="margin: 0; font-size: 20px;">       🏆 #{rank}º [{pontos}] pts   {rodada}</h2>
     </div>
     <hr style="margin-top: 5px; margin-bottom: 15px;">
     """
