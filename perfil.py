75acd9a3c4cb159a500125ae17273c8c4a602696
Carlos Santos <carlos.santosx85@hotmail.com>
2025-08-16 10:03:48 -0300

    Add files via upload

--- perfil.py
+++ perfil.py
@@ -36,6 +36,6 @@ def perfil_page():
     # Dados do usuário
     st.write(f"**Nome:** {usuario.get('nome', '---')}")
     st.write(f"**ID:** {usuario.get('seq', '---')}")
-    st.write(f" {nome_arquivo_foto}")
+    st.write(f"dd {nome_arquivo_foto}")
 
     
