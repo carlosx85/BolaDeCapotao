df1cb3292514f7273790ee7e4b0f6195e2769217
Carlos Santos <carlos.santosx85@hotmail.com>
2025-08-15 21:45:20 -0300

    Add files via upload

--- perfil.py
+++ perfil.py
@@ -12,7 +12,7 @@ def perfil_page():
     # Exibe a foto de perfil
     nome_arquivo_foto = usuario.get("foto")  # ex.: "carlos.jpg"
     if nome_arquivo_foto:
-        url_foto = f"https://boladecapotao.com/Palpiteiros/{nome_arquivo_foto}.png"
+        url_foto = f"https://boladecapotao.com/Palpiteiros/{nome_arquivo_foto}"
         st.image(url_foto, width=150, caption=usuario.get("nome", "Usuário"))
     else:
         st.info("📷 Nenhuma foto cadastrada.")
@@ -20,3 +20,4 @@ def perfil_page():
     # Dados do usuário
     st.write(f"**Nome:** {usuario.get('nome', '---')}")
     st.write(f"**ID:** {usuario.get('seq', '---')}")
+    
