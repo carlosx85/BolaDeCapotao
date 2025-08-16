a713f67f4bc50fb3716aee0a0b8187d215e66b58
Carlos Santos <carlos.santosx85@hotmail.com>
2025-08-16 10:09:20 -0300

    Add files via upload

--- perfil.py
+++ perfil.py
@@ -13,7 +13,7 @@ def perfil_page():
     st.title(f"👤 {usuario.get('nome', '---')}")
 
     # Foto padrão (quando não encontra no servidor)
-    FOTO_PADRAO = f"https://boladecapotao.com/Palpiteiros/{usuario.get}.png"
+    FOTO_PADRAO = f"https://boladecapotao.com/Palpiteiros/{usuario.get('nome', '---')}.png"
 
     # Pega o nome do arquivo e remove barras/espacos extras
     nome_arquivo_foto = usuario.get("foto", "").strip().lstrip("/")
