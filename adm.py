209f8516d74ac4605321de909e2f1570f08f4c15
Carlos Santos <carlos.santosx85@hotmail.com>
2025-08-24 10:55:22 -0300

    Add files via upload

--- adm.py
+++ adm.py
@@ -1,12 +1,9 @@
 import streamlit as st
 import requests
 
-def adml_page():  
+def adm_page():  
 
-    usuario = st.session_state.get("usuario_logado", {})
-
-    st.title(f"👤 {usuario.get('nome', '---')}")
-
+ 
 
 
     
