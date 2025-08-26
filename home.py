a4cb7863c27d168e718d870276ee57df9fdd7829
Carlos Santos <carlos.santosx85@hotmail.com>
2025-08-26 10:19:39 -0300

    Add files via upload

--- home.py
+++ home.py
@@ -323,7 +323,15 @@ def home_page():
         df = pd.DataFrame(dados)
 
         # Mostra o DataFrame com destaque por rank
-        st.dataframe(aplicar_estilo(df), use_container_width=True)       
+        st.dataframe(aplicar_estilo(df), use_container_width=True)      
+        
+        
+        
+        
+        col1, col2, col3 = st.columns(3)
+        col1.metric("Temperature", "70 °F", "1.2 °F")
+        col2.metric("Wind", "9 mph", "-8%")
+        col3.metric("Humidity", "86%", "4%") 
                    
 
                                 
