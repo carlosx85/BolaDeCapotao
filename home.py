d328940018abd9e0b8b6af32293a40dfe47bdb56
Carlos Santos <carlos.santosx85@hotmail.com>
2025-08-26 10:23:07 -0300

    Add files via upload

--- home.py
+++ home.py
@@ -329,9 +329,9 @@ def home_page():
         
         
         col1, col2, col3 = st.columns(3)
-        col1.metric("Temperature", "70 °F", "1.2 °F")
-        col2.metric("Wind", "9 mph", "-8%")
-        col3.metric("Humidity", "86%", "4%") 
+        col1.metric("Colocação", "70 °F", "1.2 °F")
+        col2.metric("Pontos", "9 mph", "-8%")
+        col3.metric("Rodada", F"{rodadaativa}%", "4%") 
                    
 
                                 
