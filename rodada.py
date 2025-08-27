e699ea25ae8726fe8a970a2c473463ed7c6ec56e
Carlos Santos <carlos.santosx85@hotmail.com>
2025-08-27 19:24:58 -0300

    Add files via upload

--- rodada.py
+++ rodada.py
@@ -1,10 +1,8 @@
 import streamlit as st
 import requests
-from db import  get_rodadas,
+ 
 
 
 def adm_rodada():  
     
-    usuario = st.session_state.get("usuario_logado", {})
-
-    st.title(f"👤 {usuario.get('nome', '---')}")
+ 
