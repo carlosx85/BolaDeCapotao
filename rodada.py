ef303a9bd4f87496b3567556be4b438f40beb0d0
Carlos Santos <carlos.santosx85@hotmail.com>
2025-08-27 19:34:07 -0300

    Add files via upload

--- rodada.py
+++ rodada.py
@@ -7,7 +7,12 @@ from db import get_rodadas
 def adm_rodada():
     st.write("Informações da rodada aqui.") 
     usuario = st.session_state.get("usuario_logado", {})
-
     st.title(f"👤 {usuario.get('nome', '---')}")
     
  
+    rodadas = get_rodadas()
+
+    for rodada in rodadas:
+        rodada_nome = f"Rodada {rodada['Rodada']} - {rodada['StatusRodada']}"
+        if rodada["Rodada_Ativa_SN"] == "S":
+            rodada_nome += " ✅ (Ativa)"
