1fb57f886155e1b25fb3f1166e1dc92b43e95f74
Carlos Santos <carlos.santosx85@hotmail.com>
2025-09-03 09:22:01 -0300

    Add files via upload

--- rodada.py
+++ rodada.py
@@ -41,27 +41,36 @@ def adm_rodada():
                 # busca só quando precisa
                 jogosx = get_jogos(id_usuario, rodada["Rodada"])
 
-                if jogosx:
-                    for jogo in jogosx:
-                        pontos = jogo["Pontos"]
-                        mandante = jogo["Mandante"]
-                        visitante = jogo["Visitante"]
 
-                        palpite_mandante_gol = jogo["Palpite_Mandante_Gol"] if jogo["Palpite_Mandante_Gol"] is not None else "-"
-                        palpite_visitante_gol = jogo["Palpite_Visitante_Gol"] if jogo["Palpite_Visitante_Gol"] is not None else "-"
-                        mandante_gol = jogo["Mandante_Gol"] if jogo["Mandante_Gol"] is not None else ""
-                        visitante_gol = jogo["Visitante_Gol"] if jogo["Visitante_Gol"] is not None else ""
+                           
+    
+                            
 
-                        st.markdown(
-                            f"""
-                            <div style="display: flex; align-items: center; justify-content: center; gap: 10px; margin-bottom: 10px;">
-                                <img src="https://boladecapotao.com/times/{mandante.lower()}.png" width="30" />
-                                <span style="font-size: 18px;">{palpite_mandante_gol} x {palpite_visitante_gol}</span>
-                                <img src="https://boladecapotao.com/times/{visitante.lower()}.png" width="30" />
-                            </div>
-                            """,
-                            unsafe_allow_html=True
-                        )
+                for jogo in jogosx:
+                    pontos = jogo["Pontos"]
+                    mandante = jogo["Mandante"]
+                    visitante = jogo["Visitante"] 
+                    palpite_mandante_gol = jogo["Palpite_Mandante_Gol"] if jogo["Palpite_Mandante_Gol"] is not None else "-"
+                    palpite_visitante_gol = jogo["Palpite_Visitante_Gol"] if jogo["Palpite_Visitante_Gol"] is not None else "-"
+                    mandante_gol = jogo["Mandante_Gol"] if jogo["Mandante_Gol"] is not None else ""
+                    visitante_gol = jogo["Visitante_Gol"] if jogo["Visitante_Gol"] is not None else ""
+                    st.markdown(
+                        f"""
+                        <div style="display: flex; align-items: center; justify-content: center; gap: 10px; margin-bottom: 10px;">
+                            <span style="font-size: 14px; font-weight: ; text-align: center;">{mandante_gol} x {visitante_gol}&nbsp; &nbsp; </span>
+                            <img src="https://boladecapotao.com/times/{mandante.lower()}.png" width="30"  />
+                            <span style="font-size: 14px; font-weight: ;">{palpite_mandante_gol} x {palpite_visitante_gol}</span>
+                            <img src="https://boladecapotao.com/times/{visitante.lower()}.png" width="30" />
+                            <span style="font-size: 14px; font-weight: ; text-align: center;"> &nbsp; &nbsp; &nbsp; &nbsp; {pontos if pontos is not None else ""} </span> 
+                        </div>
+                        """,
+                        unsafe_allow_html=True
+        )
+
+
+
+
+
             else:
                 st.write("🚫 **Mostrar**")
 
